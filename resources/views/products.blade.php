9318f837394509a0243679e100733b73906de865
GuerfiHamza <43896427+GuerfiHamza@users.noreply.github.com>
2021-11-20 15:49:14 +0100

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -30,7 +30,7 @@
         </div>
         <div class="hidden lg:block w-1/4 px-3">
           <div class="mb-6 py-10 px-12 bg-gray-50">
-            <h3 class="mb-8 text-2xl font-bold font-heading">Category</h3>
+            <h3 class="mb-8 text-2xl font-bold font-heading text-yellow-600">Category</h3>
             <ul>
                 @foreach ($categories as $category)
 
@@ -44,10 +44,10 @@
         <div class="w-full lg:w-3/4 px-3">
           <div class="flex flex-wrap -mx-3">
               @foreach ($products as $product)
-            <div class="w-full sm:w-1/2 md:w-1/3 px-3 mb-8 border border-yellow-500">
-              <div class="p-6 bg-gray-50">
+            <div class="w-full sm:w-1/2 md:w-1/3 px-3 mb-8">
+              <div class="p-6 bg-gray-50  rounded-md ">
                 <a class="block px-6 mt-6 mb-2 " href="{{ route('products.show', $product->slug) }}">
-                  <img class="mb-5 mx-auto h-56 w-full object-contain " src="{{ URL::asset('storage/'.$product->image) }} " alt="{{ $product->name }}">
+                  <img class="mb-5 mx-auto h-56 w-full contain " src="{{ URL::asset('storage/'.$product->image) }} " alt="{{ $product->name }}">
                   <h3 class="mb-2 text-xl font-bold font-heading">{{ $product->name }}</h3>
                   <p class="text-sm  font-heading ">
                    {{ $product->details }}
