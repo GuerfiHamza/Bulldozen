ac300a43dbe567aa8960156d7902145a736e31af
GuerfiHamza <43896427+GuerfiHamza@users.noreply.github.com>
2021-12-04 17:43:14 +0100

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,156 +3,192 @@
 @section('title', 'Contact')
 
 @section('content')
-<section class="py-10 ">
-    <div class="container mx-auto px-4">
-      <div class="flex flex-wrap items-center justify-center -mx-4">
-        <div class="w-full lg:w-1/2 px-4 order-1 lg:order-0">
-          <h2 class="mb-4 text-4xl md:text-5xl font-bold font-heading">Entrer en contact avec nous !</h2>
-          <p class="mb-8 text-gray-500 leading-loose">Nous serons heureux de vous entendre.</p>
+    <section class="py-10 ">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-wrap items-center justify-center -mx-4">
+                <div class="w-full lg:w-1/2 px-4 order-1 lg:order-0">
+                    <h2 class="mb-4 text-4xl md:text-5xl font-bold font-heading">Entrer en contact avec nous !</h2>
+                    <p class="mb-8 text-gray-500 leading-loose">Nous serons heureux de vous entendre.</p>
+                </div>
+                <div class="w-full lg:w-1/2 px-4 order-0 lg:order-1 mb-8 lg:mb-0 ">
+                    <img src="img/bgcon.png" class="rounded-xl bg-cover ">
+                </div>
+            </div>
         </div>
-        <div class="w-full lg:w-1/2 px-4 order-0 lg:order-1 mb-8 lg:mb-0 ">
-          <img src="img/bgcon.png" class="rounded-xl bg-cover ">
-        </div>
-      </div>
-    </div>
-  </section>
-<section class="py-20">
-    <div class="container px-4 mx-auto">
-      <div class="max-w-2xl lg:max-w-3xl mx-auto">
-        <div class="flex flex-wrap -mx-3 text-center">
-            <div class="w-full lg:w-1/3 px-3 mb-12">
-                <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                </svg>
-                <div class="leading-relaxed">
-                  <span class="text-sm text-gray-400">Ouled Moussa</span>
-                  <p>Cite Med Sahnoune Ouled Moussa W. Boumerdes</p>
-                </div>
-              </div>
-          <div class="w-1/2 lg:w-1/3 px-3 mb-12">
-            <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-            </svg>
-            <div class="leading-relaxed">
-              <span class="text-sm text-gray-400">Téléphone</span>
-              <p>+213 661 820 560</p>
-              <p>+213 664 18 18 44</p>
-            </div>
-          </div>
-          <div class="w-1/2 lg:w-1/3 px-3 mb-12">
-            <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-            </svg>
-            <div class="leading-relaxed">
-              <span class="text-sm text-gray-400">E-mail</span>
-              <p>contact@bulldozen-algerie.com</p>
-            </div>
-          </div>
-
-
-        </div>
-        <div class="flex flex-wrap -mx-3 text-center">
-            <div class="w-full lg:w-1/3 px-3 mb-12">
-                <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                </svg>
-                <div class="leading-relaxed">
-                  <span class="text-sm text-gray-400">Oued Smar </span>
-                  <p>Micro Zone d’Activité N°15 Oued Smar W. Alger</p>
-                </div>
-              </div>
-            <div class="w-1/2 lg:w-1/3 px-3 mb-12">
-              <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-              </svg>
-              <div class="leading-relaxed">
-                <span class="text-sm text-gray-400">Téléphone</span>
-                <p>+213 671 366 061 </p>
-                <p>+213 671 366 062</p>
-              </div>
-            </div>
-            <div class="w-1/2 lg:w-1/3 px-3 mb-12">
-              <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-              </svg>
-              <div class="leading-relaxed">
-                <span class="text-sm text-gray-400">E-mail</span>
-                <p>contact@bulldozen-algerie.com</p>
-            </div>
-            </div>
-
-
-          </div>
-        <div>
-            @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div><br />
-        @endif
-          <form action="{{ route('mailContactForm') }}" method="post">
-            @csrf
-
-            <div class="flex flex-wrap -mx-3">
-              <div class="w-full lg:w-1/2 px-3 mb-4 lg:mb-0">
-                <div class="mb-4">
-                  <input class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none" type="text" name="name" placeholder="Nom & Prénom">
-                </div>
-                <div class="mb-4">
-                  <input class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none" type="text" name="subject" placeholder="Sujet">
-                </div>
-                <div class="mb-4">
-                  <input class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none" type="email" name="email" placeholder="name@example.com">
-                </div>
-                <div class="mb-4">
-                    <input class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none" type="tel" name="tel" placeholder="Téléphone">
-                  </div>
-                  <div class="flex flex-wrap mb-4">
-                    <div class="captcha">
-                        <span>{!! captcha_img() !!}</span>
-
+    </section>
+    <section class="py-20">
+        <div class="container px-4 mx-auto">
+            <div class="max-w-2xl lg:max-w-3xl mx-auto">
+                <div class="flex flex-wrap -mx-3 text-center">
+                    <div class="w-full lg:w-1/3 px-3 mb-12">
+                        <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z">
+                            </path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                        </svg>
+                        <div class="leading-relaxed">
+                            <span class="text-sm text-gray-400">Ouled Moussa</span>
+                            <p>Cite Med Sahnoune Ouled Moussa W. Boumerdes</p>
+                        </div>
                     </div>
-                    <div style="margin-left: 10px">
-                        <button type="button" id="reload">
-                            ↻
-                        </button>
+                    <div class="w-1/2 lg:w-1/3 px-3 mb-12">
+                        <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
+                            </path>
+                        </svg>
+                        <div class="leading-relaxed">
+                            <span class="text-sm text-gray-400">Téléphone</span>
+                            <p>+213 661 820 560</p>
+                            <p>+213 664 18 18 44</p>
+                        </div>
                     </div>
+                    <div class="w-1/2 lg:w-1/3 px-3 mb-12">
+                        <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
+                            </path>
+                        </svg>
+                        <div class="leading-relaxed">
+                            <span class="text-sm text-gray-400">E-mail</span>
+                            <p>contact@bulldozen-algerie.com</p>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div class="mb-4">
-                    <input class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none" type="text" name="captcha" placeholder="Enter Captcha">
-                  </div>
-              </div>
-              <div class="w-full lg:w-1/2 px-3"><textarea class="w-full h-full p-4 text-xs font-semibold leading-none resize-none bg-gray-50 rounded outline-none" type="text" name="message" placeholder="Message..."></textarea></div>
+                <div class="flex flex-wrap -mx-3 text-center">
+                    <div class="w-full lg:w-1/3 px-3 mb-12">
+                        <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z">
+                            </path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                        </svg>
+                        <div class="leading-relaxed">
+                            <span class="text-sm text-gray-400">Oued Smar </span>
+                            <p>Micro Zone d’Activité N°15 Oued Smar W. Alger</p>
+                        </div>
+                    </div>
+                    <div class="w-1/2 lg:w-1/3 px-3 mb-12">
+                        <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
+                            </path>
+                        </svg>
+                        <div class="leading-relaxed">
+                            <span class="text-sm text-gray-400">Téléphone</span>
+                            <p>+213 671 366 061 </p>
+                            <p>+213 671 366 062</p>
+                        </div>
+                    </div>
+                    <div class="w-1/2 lg:w-1/3 px-3 mb-12">
+                        <svg class="mb-6 h-8 w-8 mx-auto text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
+                            </path>
+                        </svg>
+                        <div class="leading-relaxed">
+                            <span class="text-sm text-gray-400">E-mail</span>
+                            <p>contact@bulldozen-algerie.com</p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div>
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div><br />
+                    @endif
+                    <form action="{{ route('mailContactForm') }}" method="post">
+                        @csrf
+
+                        <div class="flex flex-wrap -mx-3">
+                            <div class="w-full lg:w-1/2 px-3 mb-4 lg:mb-0">
+                                <div class="mb-4">
+                                    <input
+                                        class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
+                                        type="text" name="name" placeholder="Nom & Prénom">
+                                </div>
+                                <div class="mb-4">
+                                    <input
+                                        class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
+                                        type="text" name="subject" placeholder="Sujet">
+                                </div>
+                                <div class="mb-4">
+                                    <input
+                                        class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
+                                        type="email" name="email" placeholder="name@example.com">
+                                </div>
+                                <div class="mb-4">
+                                    <input
+                                        class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
+                                        type="tel" name="tel" placeholder="Téléphone">
+                                </div>
+                                <div class="mb-4">
+
+                                    <div class="flex flex-wrap mb-4">
+                                        <div class="captcha">
+                                            <span>{!! captcha_img() !!}</span>
+
+                                        </div>
+                                        <div style="margin-left: 10px">
+                                            <button type="button" id="reload">
+                                                ↻
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <input
+                                        class="w-full p-4 text-xs font-semibold leading-none bg-gray-50 rounded outline-none"
+                                        type="text" name="captcha" placeholder="Enter Captcha">
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-1/2 px-3"><textarea
+                                    class="w-full h-full p-4 text-xs font-semibold leading-none resize-none bg-gray-50 rounded outline-none"
+                                    type="text" name="message" placeholder="Message..."></textarea></div>
+                        </div>
+
+
+                        <div class="flex mt-5 items-center justify-end">
+
+                            <button
+                                class="py-4 px-8 text-sm text-white font-semibold leading-none bg-yellow-600 hover:bg-yellow-700 rounded"
+                                type="submit">Envoyer</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-
-            <div class="flex mt-5 items-center justify-end">
-
-              <button class="py-4 px-8 text-sm text-white font-semibold leading-none bg-yellow-600 hover:bg-yellow-700 rounded" type="submit">Envoyer</button>
-            </div>
-          </form>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 
 @section('scripts')
-<script type="text/javascript">
-    $('#reload').click(function () {
-        $.ajax({
-            type: 'GET',
-            url: 'contact/reload-captcha',
-            success: function (data) {
-                $(".captcha span").html(data.captcha);
-            }
+    <script type="text/javascript">
+        $('#reload').click(function() {
+            $.ajax({
+                type: 'GET',
+                url: 'contact/reload-captcha',
+                success: function(data) {
+                    $(".captcha span").html(data.captcha);
+                }
+            });
         });
-    });
-
-</script>
+    </script>
 @endsection
 @stop
