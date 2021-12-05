793baf45ec3e8e01cadda1121828783973bd867f
GuerfiHamza <43896427+GuerfiHamza@users.noreply.github.com>
2021-12-05 15:00:42 +0100

    EDITS05

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,8 +3,20 @@
 @section('title', 'A propos')
 
 @section('content')
-<div class="container mx-auto py-20">
-{!! $about->description !!}
+<section class="relative py-20">
+    <div class="relative container px-4 mx-auto">
+      <div class="flex flex-wrap items-center -mx-4">
 
-</div>
+        <div class="relative w-full  lg:w-1/2 px-4">
+          <img class="rounded-xl object-cover" src="{{ URL::asset('storage/' . $about->image) }}" alt="">
+        </div>
+        <div class="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
+            <div class="max-w-lg">
+              <p class="text-xl text-gray-500">{!! $about->description !!}</p>
+            </div>
+          </div>
+      </div>
+    </div>
+    <img class="hidden xl:block absolute top-0 right-0 mt-52" src="zeus-assets/icons/dots/yellow-dot-right-shield.svg" alt="">
+  </section>
 @endsection
