6c351ef56c2eaf1c035d111717ceb2c792fee8ec
GuerfiHamza <43896427+GuerfiHamza@users.noreply.github.com>
2021-12-06 18:36:18 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
     <link rel="icon" href="../img/favicon.ico"/>
     <meta name="description" content="{{config('site.desc')}}">
+    <meta name="keywords" content="{{config('site.keywords')}}">
     <link rel="stylesheet" type="text/css"
     href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
