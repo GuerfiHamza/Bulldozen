2384199a005e41f0c9af3bf08fd8d1b99a22c1b0
GuerfiHamza <43896427+GuerfiHamza@users.noreply.github.com>
2021-12-06 18:35:12 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BullDozen - @yield('title')</title>
+    <title>BULLDOZEN - @yield('title')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
